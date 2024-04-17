c296c34fe99dd8ac4b71976527bd44b3ab27c248
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2024-04-16 22:51:14 -0700

    ds

--- folio.js
+++ folio.js
@@ -1,7 +1,36 @@
 document.getElementById('anc').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default action of the anchor link
+    event.preventDefault();
     const targetElement = document.getElementById('unc');
     if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the target element
+        targetElement.scrollIntoView({ behavior: 'smooth' }); 
     }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the modal and the close button
+    var modal = document.getElementById("resume-modal");
+    var closeButton = document.querySelector(".close");
+
+    // Find the resume link
+    var resumeLink = document.querySelector(".resume-link");
+
+    // Add event listener to open the modal when resume link is clicked
+    resumeLink.addEventListener("click", function() {
+        modal.style.display = "block";
+        document.body.classList.add("dimmed-background");
+    });
+
+    // Add event listener to close the modal when the close button is clicked
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+        document.body.classList.remove("dimmed-background");
+    });
+
+    // Close the modal when clicking outside of it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+            document.body.classList.remove("dimmed-background");
+        }
+    };
 });
